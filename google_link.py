c37f112228a1975aeb93a4607bf6837039578722
Kavin <kavin.friendship@gmail.com>
2017-07-27 09:06:44 +0530

    Complete code

--- google_link.py
+++ google_link.py
@@ -1,6 +1,12 @@
-import sys,bs4,requests,webbrowser
+import sys,bs4,requests,webbrowser,re
 url='https://www.google.co.in/search?q='+' '.join(sys.argv[1:])
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 webbrowser.open(url) 
-resp=requests.get(url)
+resp=requests.get(url,headers=headers)
 soup_obj=bs4.BeautifulSoup(resp.text)
-type(soup_obj)
+#print('RESP',resp.text)
+#type(soup_obj)
+links=soup_obj.select('.r a')
+for link in links:
+    webbrowser.open(link.get('href'))
+    #print(res.group())
